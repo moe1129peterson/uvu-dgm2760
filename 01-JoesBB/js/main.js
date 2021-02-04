e838e813c4fe57bb50e31d71bbece86ce74cb125
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-02-04 15:52:59 -0700

    Initial commit

---
+++ 01-JoesBB/js/main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('company').innerHTML = "Momo's Bed and Breakfast"
+
+document.querySelector('#slogan').innerText = "Best B&B in Japan"
+
+let userName = prompt("What is your name?")
+
+let message = `Hello ${userName}, welcome to the best lodging in Japan!`
+
+document.querySelector('#greeting').innerText = message
