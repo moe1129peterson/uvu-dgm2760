e19c97d4f5963f5cb520583ec7ba8df0cbd5a305
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-04-22 15:59:26 -0600

    Improved css and fixed nav links

--- 07-Pizza_Emporium/main.js
+++ 07-Pizza_Emporium/main.js
@@ -1,5 +1,3 @@
-// When function becomes part of an object it is called a method.
-
 let message 
 
 const pizza = {
@@ -8,7 +6,7 @@ const pizza = {
     topping: 'pepperoni',
     buildPizza: function() {
         console.log('build pizza method has been called')
-        message = `Baking a ${pizza.size} pizza on ${pizza.crust} crust with ${pizza.topping} and
+        message = `Baking a ${pizza.size} pizza on some ${pizza.crust} crust with high-quality ${pizza.topping} and
         cheese just for you.`
         document.querySelector('#feedback').textContent = message
     },
@@ -28,43 +26,26 @@ const pizza = {
 }
 
 
-// old version
-// let thinButton = document.querySelector('#thin')
-// thinButton.addEventListener('click',function () {
-//    return pizza.crust = 'thin'
-// })
 
-
-// addEventListener takes two arguments, first is a string that is the event you want to listen to.
-// Second argument, takes a function.
-// Crust Buttons
+// Crust Option Buttons
 document.querySelector('#thin').addEventListener('click', () => pizza.crust = 'thin')
 
 document.querySelector('#thick').addEventListener('click', () => pizza.crust = 'thick')
 
-// Topping Buttons
+// Topping Option Buttons
 
 document.querySelector('#sausage').addEventListener('click', () => pizza.topping = 'sausage')
 
 document.querySelector('#pepperoni').addEventListener('click', () => pizza.topping = 'pepperoni')
 
-// Size Buttons
+// Size Option Buttons
 
 document.querySelector('#small').addEventListener('click', () => pizza.size = 'small')
 
 document.querySelector('#large').addEventListener('click', () => pizza.size = 'large')
 
-
-
-
-// old version
-// let buildButton = document.querySelector('#build')
-// buildButton.addEventListener('click', function() {
-//     pizza.buildPizza()
-// })
+// Order Option Buttons
 
 document.querySelector('#build').addEventListener('click', pizza.buildPizza)
 
 document.querySelector('#shopping').addEventListener('click', pizza.shoppingList)
-
-//hook up the shoppingList method call here
